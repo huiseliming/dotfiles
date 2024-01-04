b08018e50c03aa1511c12530400b9e708633b23e
huiseliming <huiseliming@qq.com>
2024-01-04 16:43:44 +0800

    Update install_cpp_development_environment.sh

--- install_cpp_development_environment.sh
+++ install_cpp_development_environment.sh
@@ -1,21 +1,15 @@
 #!/bin/bash -v
 
-snap install code --classic
-
 cd ~ || exit
-git clone https://github.com/microsoft/vcpkg || exit
-sudo apt-get install build-essential tar curl zip unzip || exit
-./vcpkg/bootstrap-vcpkg.sh || exit
 
+snap install code --classic || exit
+
+sudo apt install git clang cmake ninja-build gdb || exit
+
+sudo apt install build-essential tar curl zip unzip || exit
+git clone https://github.com/microsoft/vcpkg || exit
+./vcpkg/bootstrap-vcpkg.sh || exit
 echo "export VCPKG_ROOT=~/vcpkg" >> ~/.bashrc || exit
 
-sudo apt install clang || exit
-sudo apt install gdb || exit
-sudo apt install cmake || exit
-sudo apt install ninja-build || exit
-
-sudo apt install pkg-config || exit # vcpkg boost 
-sudo apt install autoconf autoconf-archive || exit # vcpkg boost
+sudo apt install pkg-config autoconf autoconf-archive || exit # vcpkg boost 
 sudo apt install libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev pkg-config || exit # vcpkg glfw3
-
-
