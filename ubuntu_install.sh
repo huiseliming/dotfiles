8147de3d6273d2c6feec3354904b2b04cbfcd81d
huiseliming <huiseliming@qq.com>
2024-01-05 09:24:11 +0800

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,8 +1,9 @@
+
 #!/bin/bash -v
 
 sudo add-apt-repository universe || exit
 sudo apt install ibus-libpinyin || exit
 sudo apt install gnome-tweaks -y || exit
-
+sudo apt install gnome-shell-extension-manager || exit
 
 
