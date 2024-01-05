ddd34e815abb1b1a666965ee54511b330cd00517
huiseliming <huiseliming@qq.com>
2024-01-05 09:32:05 +0800

    WIP

--- install_cpp_development_environment.sh
+++ install_cpp_development_environment.sh
@@ -4,12 +4,15 @@ cd ~ || exit
 
 snap install code --classic || exit
 
-sudo apt install git clang cmake ninja-build gdb || exit
+sudo apt install git clang cmake ninja-build gdb -y || exit
 
-sudo apt install build-essential tar curl zip unzip || exit
+sudo apt install build-essential tar curl zip unzip -y || exit
 git clone https://github.com/microsoft/vcpkg || exit
 ./vcpkg/bootstrap-vcpkg.sh || exit
 echo "export VCPKG_ROOT=~/vcpkg" >> ~/.bashrc || exit
 
-sudo apt install pkg-config autoconf autoconf-archive || exit # vcpkg boost 
-sudo apt install libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev pkg-config || exit # vcpkg glfw3
+sudo apt install pkg-config autoconf autoconf-archive -y  || exit # vcpkg boost 
+sudo apt install libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev pkg-config -y || exit # vcpkg glfw3
+
+git config --global user.email "huiseliming@qq.com"
+git config --global user.name "huiseliming"
