f1e16193b768807400a1188b778a93a6d5f64920
huiseliming <huiseliming@qq.com>
2024-01-07 20:04:12 +0800

    Update windows_install.bat

--- windows_install.bat
+++ windows_install.bat
@@ -1,4 +1,3 @@
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 rem fix WARNING: Not setting tab completion: Profile file does not exist at 'C:\Users\huiseliming\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1'.
 rem @'
@@ -9,4 +8,6 @@ rem   Import-Module "$ChocolateyProfile"
 rem }
 rem '@ | Add-Content -Path $profile
 
-choco install git vscode
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install git cmake ninja vscode
