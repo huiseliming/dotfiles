74f71e137ee2e2dab134329d33066360f1859d51
huiseliming <huiseliming@qq.com>
2024-01-09 21:42:36 +0800

    Update windows_install.bat

--- windows_install.bat
+++ windows_install.bat
@@ -1,12 +1,5 @@
-
-rem fix WARNING: Not setting tab completion: Profile file does not exist at 'C:\Users\huiseliming\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1'.
-@'
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-'@ | Add-Content -Path $profile
-
+:: install choco
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
 choco install git cmake ninja vscode
+:: 改回win10的右键菜单
+reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
