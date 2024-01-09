c047ebc940e0e3a4dd229c78885fc4ccd671d892
huiseliming <huiseliming@qq.com>
2024-01-09 11:10:58 +0800

    Update windows_install.bat

--- windows_install.bat
+++ windows_install.bat
@@ -1,12 +1,12 @@
 
 rem fix WARNING: Not setting tab completion: Profile file does not exist at 'C:\Users\huiseliming\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1'.
-rem @'
-rem # Chocolatey profile
-rem $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-rem if (Test-Path($ChocolateyProfile)) {
-rem   Import-Module "$ChocolateyProfile"
-rem }
-rem '@ | Add-Content -Path $profile
+@'
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+'@ | Add-Content -Path $profile
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
