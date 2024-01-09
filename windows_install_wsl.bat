c0f3098e36e7a008ae8b8c85d9c68317975fd966
huiseliming <huiseliming@qq.com>
2024-01-09 21:29:42 +0800

    Update windows_install_wsl.bat

--- windows_install_wsl.bat
+++ windows_install_wsl.bat
@@ -24,29 +24,29 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
 ::--------------------------------------
 
-# Enable Virtual Machine Platform feature
+::Enable Virtual Machine Platform feature
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
-# Enable WSL feature
+::Enable WSL feature
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 
-# Enable Microsoft-Hyper-V feature
-# dism.exe /online /enable-feature /featurename:Microsoft-Hyper-V /all /norestart
+::Enable Microsoft-Hyper-V feature
+::dism.exe /online /enable-feature /featurename:Microsoft-Hyper-V /all /norestart
 
-# Restart Win 10 Machine
+::Restart Win 10 Machine
 
-# To set the WSL default version to 2.
-# wsl --set-default-version 2
+::To set the WSL default version to 2.
+::wsl --set-default-version 2
 
-# Installed linux distribution
-# wsl --list --online
-# wsl --install -d Ubuntu-22.04
+::Installed linux distribution
+::wsl --list --online
+::wsl --install -d Ubuntu-22.04
 
-# netsh interface portproxy
-# netsh interface ip add address "vEthernet (WSL)" 172.172.172.1 255.255.255.0
-# netsh interface portproxy add v4tov4 listenport=1111 listenaddress=0.0.0.0 connectport=1111 connectaddress=172.172.172.172
-# netsh interface portproxy show v4tov4
+::netsh interface portproxy
+::netsh interface ip add address "vEthernet (WSL)" 172.172.172.1 255.255.255.0
+::netsh interface portproxy add v4tov4 listenport=1111 listenaddress=0.0.0.0 connectport=1111 connectaddress=172.172.172.172
+::netsh interface portproxy show v4tov4
 
-# exec on Ubuntu-22.04
-#sudo ip addr add 172.172.172.172/24 broadcast 172.172.172.255 dev eth0 label eth0:1;
+::exec on Ubuntu-22.04
+::sudo ip addr add 172.172.172.172/24 broadcast 172.172.172.255 dev eth0 label eth0:1;
 
