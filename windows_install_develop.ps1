be983b1e0cdaabedc4a2a8fee0679b93e39d52fa
huiseliming <huiseliming@qq.com>
2024-01-09 09:18:18 +0800

    Update windows_install_develop.ps1

--- windows_install_develop.ps1
+++ windows_install_develop.ps1
@@ -1,15 +1,18 @@
 
-# STEP 1: Enable Virtual Machine Platform feature
+# Enable Virtual Machine Platform feature
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
-# STEP 2: Enable WSL feature
+# Enable WSL feature
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 
-# STEP 3: Restart Win 10 Machine
+# Enable Microsoft-Hyper-V feature
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
-# STEP 4: To set the WSL default version to 2.
-# Any distribution installed after this, would run on WSL 2
+# Restart Win 10 Machine
+
+# To set the WSL default version to 2.
 wsl --set-default-version 2
 
+# Installed linux distribution
 wsl --install -d Ubuntu-22.04
 
