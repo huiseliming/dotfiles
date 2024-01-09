692823613502d8db2f25236333f8df59a5704bbd
huiseliming <huiseliming@qq.com>
2024-01-09 21:14:08 +0800

    Update and rename windows_install_vcpkg.ps1 to windows_install_vcpkg.bat

---
+++ windows_install_vcpkg.bat
@@ -0,0 +1,5 @@
+
+cd c:/
+git clone https://github.com/microsoft/vcpkg
+./vcpkg/bootstrap-vcpkg.bat
+setx VCPKG_ROOT "C:\vcpkg"
--- windows_install_vcpkg.ps1
+++
@@ -1,6 +0,0 @@
-
-# 
-cd c:/
-git clone https://github.com/microsoft/vcpkg
-./vcpkg/bootstrap-vcpkg.bat
-[Environment]::SetEnvironmentVariable("VCPKG_ROOT", "C:\vcpkg", [System.EnvironmentVariableTarget]::User)
