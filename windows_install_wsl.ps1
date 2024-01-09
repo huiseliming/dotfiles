f8ba98ad5317839aa84f18e2513e9b60cd668f86
huiseliming <huiseliming@qq.com>
2024-01-09 21:07:40 +0800

    Update and rename windows_install_develop.ps1 to windows_install_wsl.ps1

--- windows_install_wsl.ps1
+++ windows_install_wsl.ps1
@@ -11,16 +11,16 @@ dism.exe /online /enable-feature /featurename:Microsoft-Hyper-V /all /norestart
 # Restart Win 10 Machine
 
 # To set the WSL default version to 2.
-wsl --set-default-version 2
+# wsl --set-default-version 2
 
 # Installed linux distribution
 # wsl --list --online
-wsl --install -d Ubuntu-22.04
+# wsl --install -d Ubuntu-22.04
 
 # netsh interface portproxy
-netsh interface ip add address "vEthernet (WSL)" 172.172.172.1 255.255.255.0
-netsh interface portproxy add v4tov4 listenport=1111 listenaddress=0.0.0.0 connectport=1111 connectaddress=172.172.172.172
-netsh interface portproxy show v4tov4
+# netsh interface ip add address "vEthernet (WSL)" 172.172.172.1 255.255.255.0
+# netsh interface portproxy add v4tov4 listenport=1111 listenaddress=0.0.0.0 connectport=1111 connectaddress=172.172.172.172
+# netsh interface portproxy show v4tov4
 
 # exec on Ubuntu-22.04
 #sudo ip addr add 172.172.172.172/24 broadcast 172.172.172.255 dev eth0 label eth0:1;
