2a8051a1321213fdd33d16ffb197bc929c781aaf
huiseliming <huiseliming@qq.com>
2024-01-18 10:02:08 +0800

    Update macos.install.sh

--- macos.install.sh
+++ macos.install.sh
@@ -6,6 +6,7 @@ brew install --cask visual-studio-code
 cd ~
 git clone https://github.com/microsoft/vcpkg
 ./vcpkg/bootstrap-vcpkg.sh
+echo "export VCPKG_ROOT=~/vcpkg" >> ~/.zprofile
 
 git config --global user.email "huiseliming@qq.com"
 git config --global user.name "huiseliming"
