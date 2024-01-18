37ec10164ed86532800e37f8f6db363a7b74fce0
huiseliming <huiseliming@qq.com>
2024-01-18 08:48:04 +0800

    Create macos.install.sh

---
+++ macos.install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -v
+
+brew install --cask google-chrome
+brew install --cask visual-studio-code
+
+git config --global user.email "huiseliming@qq.com"
+git config --global user.name "huiseliming"
