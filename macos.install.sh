fe498a915e920016e008a96f9dbf51f6f19704df
huiseliming <huiseliming@qq.com>
2024-01-18 09:57:55 +0800

    Update macos.install.sh

--- macos.install.sh
+++ macos.install.sh
@@ -3,5 +3,9 @@
 brew install --cask google-chrome
 brew install --cask visual-studio-code
 
+cd ~
+git clone https://github.com/microsoft/vcpkg
+./vcpkg/bootstrap-vcpkg.sh
+
 git config --global user.email "huiseliming@qq.com"
 git config --global user.name "huiseliming"
