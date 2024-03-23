282cfcc025e957ba58f63f40cdee00ccc98d4128
huiseliming <huiseliming@qq.com>
2024-03-24 03:00:00 +0800

    Update qt_windows_dev_build.cmd

--- qt_windows_dev_build.cmd
+++ qt_windows_dev_build.cmd
@@ -3,9 +3,11 @@ choco install python
 #pip3 install html5lib
 choco install winflexbison
 choco install gperf
-choco install nodejs
+# choco install nodejs 
 
 COPY C:\ProgramData\chocolatey\bin\win_bison.exe C:\ProgramData\chocolatey\bin\bison.exe
 COPY C:\ProgramData\chocolatey\bin\win_flex.exe C:\ProgramData\chocolatey\bin\flex.exe
 
-
+mkdir C:\Qt\build_6.6.2
+cd C:\Qt\build_6.6.2
+C:\Qt\qt-everywhere-src-6.6.2\configure.bat -prefix C:\Qt\6.6.2\dev_msvc_x64 -webengine-proprietary-codecs -opensource
