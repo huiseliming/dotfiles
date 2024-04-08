d0a3e3be3cf7b5c46339b50e9ddaaf79479fd7bb
huiseliming <huiseliming@qq.com>
2024-04-08 22:57:08 +0800

    Update qt_macos_dev_build.sh

--- qt_macos_dev_build.sh
+++ qt_macos_dev_build.sh
@@ -5,8 +5,11 @@ brew install gperf
 brew install node
 
 
-
-
+mkdir build
+cd build
+../Src/configure -prefix /Users/huiseliming/Qt/6.5.3/universal -debug-and-release -opensource -webengine-proprietary-codecs
+#cmake --install . 不会部署debug
+ninja install 
 
 
 
