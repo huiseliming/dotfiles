a4f3bf5491772d5447a9baaf7050fe82fa3c8b4f
huiseliming <huiseliming@qq.com>
2024-04-09 23:09:14 +0800

    Update qt_macos_dev_build.sh

--- qt_macos_dev_build.sh
+++ qt_macos_dev_build.sh
@@ -1,9 +1,5 @@
 python3 -m pip install html5lib
-brew install bison
-brew install flex
-brew install gperf
-brew install node
-
+brew install cmake ninja bison flex gperf node
 
 mkdir build
 cd build
