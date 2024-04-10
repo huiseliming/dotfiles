4e30c18fcdf44d36977914c1c82dd58c88d99c5a
huiseliming <huiseliming@qq.com>
2024-04-10 23:12:33 +0800

    Update qt_macos_dev_build.sh

--- qt_macos_dev_build.sh
+++ qt_macos_dev_build.sh
@@ -6,6 +6,7 @@ sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
 
 mkdir build
 cd build
+../qt-everywhere-src-6.7.0/configure -prefix /Users/huiseliming/Qt/6.7.0/macOS -debug-and-release -opensource -webengine-proprietary-codecs
 ../qt-everywhere-src-6.6.3/configure -prefix /Users/huiseliming/Qt/6.6.3/macOS -debug-and-release -opensource -webengine-proprietary-codecs
 #cmake --install . 不会部署debug
 ninja install 
